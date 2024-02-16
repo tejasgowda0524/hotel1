3346f6fc53063e655bd85a4349660b754365f0bd
TEJAS <tejasgowda0524@gmail.com>
2024-02-17 01:12:03 +0530

    added poko class of hotel

---
+++ invalidCustomer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac1
+{
+    internal class invalidCustomer : Exception
+    {
+        public invalidCustomer(string message) : base(message)
+        {
+
+        }
+    }
+
+    internal class invalidDateException : Exception
+    {
+        public invalidDateException(string message) : base(message)
+        {
+
+        }
+    }
+
+
+}
